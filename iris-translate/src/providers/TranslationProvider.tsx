a99dfcd67a3128e01786c44b622809b3823d2ccc
Elias-Manica <eliasmanica@poli.ufrj.br>
2024-07-26 14:45:23 -0300

    refactor: ensure host and remote use provider iris-translate

--- iris-translate/src/providers/TranslationProvider.tsx
+++ iris-translate/src/providers/TranslationProvider.tsx
@@ -2,27 +2,20 @@ import React, { ReactNode } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import i18nInstance from '../i18n';
 
-type TranslationProviderProps = {
+interface TranslationProviderProps {
   children: ReactNode;
-  additionalResources?: {
-    [lng: string]: { translation: { [key: string]: string } };
-  };
-};
+  additionalResources?: Record<string, any>;
+}
 
 const TranslationProvider: React.FC<TranslationProviderProps> = ({
   children,
   additionalResources,
 }) => {
-  React.useEffect(() => {
-    if (additionalResources) {
-      Object.keys(additionalResources).forEach((lng) => {
-        Object.keys(additionalResources[lng]).forEach((ns) => {
-          //@ts-ignore
-          i18nInstance.addResources(lng, ns, additionalResources[lng][ns]);
-        });
-      });
+  if (additionalResources) {
+    for (const [lang, resources] of Object.entries(additionalResources)) {
+      i18nInstance.addResources(lang, 'translation', resources.translation);
     }
-  }, [additionalResources]);
+  }
 
   return <I18nextProvider i18n={i18nInstance}>{children}</I18nextProvider>;
 };
